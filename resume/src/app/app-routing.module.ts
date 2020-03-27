3b07fa2c254bbd44acc5188761e5de1d15c2f628
Prince267 <prince.kumar@arvindinternet.com>
2020-03-27 22:11:42 +0530

    routing module

--- resume/src/app/app-routing.module.ts
+++ resume/src/app/app-routing.module.ts
@@ -2,18 +2,20 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component'
 import {AboutMeComponent} from './about-me/about-me.component'
-import {ExperienceComponent} from './experience/experience.component'
 import {ContactComponent} from './contact/contact.component'
 // import {ProjectsModule} from './projects/projects.module'
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path:'about-me', component:AboutMeComponent},
-  { path:'experience', component: ExperienceComponent},
   { path:'contact', component:ContactComponent},
   {
     path:'projects',
     loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)
+  },
+  {
+    path:'experience',
+    loadChildren:()=> import('./experience/experience.module').then(m=>m.ExperienceModule)
   }
 ];
 
--- resume/src/app/app.module.ts
+++ resume/src/app/app.module.ts
@@ -12,7 +12,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { ExperienceComponent } from './experience/experience.component';
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
@@ -21,7 +20,6 @@ import { ContactComponent } from './contact/contact.component';
     MainNavComponent,
     HomeComponent,
     AboutMeComponent,
-    ExperienceComponent,
     ContactComponent
   ],
   imports: [
