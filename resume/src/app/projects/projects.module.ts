e1bf741469088b4842aef2c8a782dd6a32e2f71a
Prince267 <prince.kumar@arvindinternet.com>
2020-03-27 13:35:41 +0530

    minor modification

--- resume/src/app/projects/projects.module.ts
+++ resume/src/app/projects/projects.module.ts
@@ -5,21 +5,12 @@ import { ProjectsRoutingModule } from './projects-routing.module';
 import {MatTableModule} from '@angular/material/table';
 import { ProjectComponent } from './project/project.component';
 import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-// import { 
-//   MatCardModule,
-//   MatListModule,
-//   MatIconModule
-// } from '@angular/material';
 
 @NgModule({
   declarations: [ProjectComponent],
   imports: [
     CommonModule,
     MatCardModule,
-    MatListModule,
-    MatIconModule,
     MatTableModule,
     ProjectsRoutingModule
   ]
