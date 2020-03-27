8351eb6a247dc38fb3704f5559b2caf11266c03e
Prince267 <prince.kumar@arvindinternet.com>
2020-03-27 12:58:27 +0530

    Projet component complete

--- resume/src/app/projects/projects.module.ts
+++ resume/src/app/projects/projects.module.ts
@@ -4,12 +4,22 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import {MatTableModule} from '@angular/material/table';
 import { ProjectComponent } from './project/project.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+// import { 
+//   MatCardModule,
+//   MatListModule,
+//   MatIconModule
+// } from '@angular/material';
 
 @NgModule({
   declarations: [ProjectComponent],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
     MatTableModule,
     ProjectsRoutingModule
   ]
