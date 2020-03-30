c9b68c73959d15c015b6a0fddd6e0237a748c1b4
Prince267 <prince.kumar@arvindinternet.com>
2020-03-30 17:36:07 +0530

    Skill component added

--- resume/src/app/skills/skills.module.ts
+++ resume/src/app/skills/skills.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { SkillsRoutingModule } from './skills-routing.module';
 import { TechnicalSkillsComponent } from './technical-skills/technical-skills.component';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [TechnicalSkillsComponent],
   imports: [
     CommonModule,
-    SkillsRoutingModule
+    MatListModule,
+    MatGridListModule,
+    SkillsRoutingModule,
+    MatCardModule
   ]
 })
 export class SkillsModule { }
--- resume/src/app/skills/technical-skills/technical-skills.component.ts
+++ resume/src/app/skills/technical-skills/technical-skills.component.ts
@@ -1,12 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Section {
+  name: string;
+  icon: string;
+}
+
+
 @Component({
   selector: 'app-technical-skills',
   templateUrl: './technical-skills.component.html',
   styleUrls: ['./technical-skills.component.css']
 })
-export class TechnicalSkillsComponent implements OnInit {
 
+
+export class TechnicalSkillsComponent implements OnInit {
+  languages: Section[] = [
+    {
+      name: 'C',
+      icon: '../../../assets/icons/c-programming.png',
+    },
+    {
+      name: 'Golang',
+      icon: '../../../assets/icons/golang.png',
+    },
+    {
+      name: 'JavaScript',
+      icon: '../../../assets/icons/javascript.png',
+    },
+    {
+      name: 'HTML5',
+      icon: '../../../assets/icons/html-5.png',
+    }
+  ];
+  Datbases: Section[] = [
+    {
+      name: 'Postgress',
+      icon: '../../../assets/icons/elephant.png',
+    },
+    {
+      name: 'MySql',
+      icon: '../../../assets/icons/mysql.png',
+    },
+    {
+      name: 'MongoDB',
+      icon: '../../../assets/icons/mongodb.png',
+    },
+  ];
+  Framework: Section[] = [
+    {
+      name: 'AngularJS',
+      icon: '../../../assets/icons/angularjs.png'
+    },
+    {
+      name: 'React JS',
+      icon: '../../../assets/icons/react.png'
+    },
+    {
+      name: 'React Native',
+      icon: '../../../assets/icons/react-native.png'
+    },
+    {
+      name: 'Node',
+      icon: '../../../assets/icons/nodejs.png'
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
