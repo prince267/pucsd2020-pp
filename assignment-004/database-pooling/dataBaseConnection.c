2aff949447ab21f297f16943fb40fefcee10f47e
Prince267 <prince.kumar@arvindinternet.com>
2020-04-14 00:41:22 +0530

    Mysql Database Connectivity zdb

--- assignment-004/database-pooling/dataBaseConnection.c
+++ assignment-004/database-pooling/dataBaseConnection.c
@@ -3,12 +3,6 @@
 #include <assert.h>
 #include <zdb.h>
 
-/*
- gcc -o select select.c -L/usr/lib -lzdb -I/usr/include/zdb -lpthread
- This example demonstrate most of the functionality of libzdb and can be compiled with a C, OBJ-C(++) or a C++ compiler.
- Compile: [gcc -std=c99|g++|clang|clang++] -o select select.c -L/<libzdb>/lib -lzdb -I/<libzdb>/include/zdb
- */
-
 int main(void)
 {
     URL_T url= URL_new("mysql://localhost:3306/test?user=test&password=p");
@@ -18,19 +12,19 @@ int main(void)
     Connection_T con = ConnectionPool_getConnection(pool);
     TRY
     {
-        Connection_execute(con, "create table User(name varchar(255))");
-        PreparedStatement_T p = Connection_prepareStatement(con, "insert into User values (?)");
-        const char *bleach[] = {
-            "prince kumae", "Sagar adhalge","Nayan Lonkar", 0};
-        for (int i = 0; bleach[i]; i++)
+        Connection_execute(con, "create table users(name varchar(255))");
+        PreparedStatement_T p = Connection_prepareStatement(con, "insert into users values (?)");
+        const char *user[] = {
+            "prince kumar", "Sagar adhalge","Nayan Lonkar", 0};
+        for (int i = 0; user[i]; i++)
         {
-            PreparedStatement_setString(p, 1, bleach[i]);
+            PreparedStatement_setString(p, 1, user[i]);
             PreparedStatement_execute(p);
         }
-        ResultSet_T r = Connection_executeQuery(con, "select name from User");
+        ResultSet_T r = Connection_executeQuery(con, "select name from users");
         while (ResultSet_next(r))
             printf("%s\n", ResultSet_getString(r, 1));
-        Connection_execute(con, "drop table User;");
+        Connection_execute(con, "drop table users;");
     }
     CATCH(SQLException)
     {
